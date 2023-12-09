4f92b79cc53acc60f050ee6622da8ed5a144c227
NecroBread <mariovyordanov@gmail.com>
2023-12-09 13:59:56 +0200

    update app error

--- src/utils/app-error.ts
+++ src/utils/app-error.ts
@@ -2,9 +2,25 @@ import { HttpStatusCode } from "./http-status-code";
 
 export class AppError extends Error {
   statusCode: HttpStatusCode;
+  /**
+   * isOperational flag that is set to true by default.
+   * It’s used to distinguish between known errors raised by us and other ones that we don’t know how to handle.
+   */
+  isOperational: boolean;
 
-  constructor(statusCode: HttpStatusCode, message: string) {
+  constructor(
+    statusCode: HttpStatusCode,
+    message: string,
+    isOperational = true,
+    stack = ""
+  ) {
     super(message);
     this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
