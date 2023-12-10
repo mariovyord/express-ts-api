239152e858776ed271017623415c72f0d21594f3
NecroBread <mariovyordanov@gmail.com>
2023-12-10 13:04:17 +0200

    add ping route

--- src/root-router.ts
+++ src/root-router.ts
@@ -3,6 +3,7 @@ import user from "./features/user/user-router";
 import article from "./features/article/article-router";
 import { HttpStatusCode } from "./utils/http-status-code";
 import { AppError } from "./utils/app-error";
+import JsonResponse from "./utils/json-response";
 
 const router = express.Router();
 
@@ -10,6 +11,17 @@ function getSubRoute(version: "v1" | "v2", subroute: string): string {
   return `/api/${version}/${subroute}`;
 }
 
+function ping(req: Request, res: Response) {
+  res.json(
+    new JsonResponse({
+      code: 200,
+      message: "API is operational",
+      data: null,
+    })
+  );
+}
+
+router.get("/api/ping", ping);
 router.use(getSubRoute("v1", "user"), user);
 router.use(getSubRoute("v1", "articles"), article);
 
